3fbc31e57a6f9f8b5b7e0238517c0c2a3c943205
Michael Teichgräber <mteichgraeber@gmx.de>
2022-08-11 11:40:58 +0200

    restructure API, using functional options, and improved naming

--- serframe.go
+++ serframe.go
@@ -10,30 +10,70 @@ import (
 )
 
 type Stream struct {
-	buf         []byte
-	echo        []byte
-	req         chan []byte
-	done        chan readResult
-	errC        chan error
-	eof         bool
-	Forward     io.Writer
-	MsgComplete func([]byte) bool
-	CheckBytes  func(bnew []byte, msg []byte) bool
+	buf          []byte
+	eof          bool
+	MsgComplete  func([]byte) bool
+	globalParams receptionParams
+	curParams    receptionParams
+
+	// internal read handler
+	req  chan []byte
+	done chan readResult
+	errC chan error
+
+	r               io.Reader
+	internalBuf     []byte
+	internalBufSize int
+	forward         io.Writer
+
+	ExitC <-chan error
 }
 
-type ReadFunc func() ([]byte, error)
-
-func NewStream(rf ReadFunc, exitC chan<- error) *Stream {
+func NewStream(r io.Reader, opts ...Option) *Stream {
 	s := new(Stream)
-	s.buf = make([]byte, 0, 256)
 	s.req = make(chan []byte)
 	s.done = make(chan readResult)
 	s.errC = make(chan error)
-	go s.handle(rf, exitC)
+	s.globalParams.tMax = time.Second
+	s.r = r
+
+	s.internalBufSize = 64
+	for _, o := range opts {
+		o(s)
+	}
+	s.internalBuf = make([]byte, s.internalBufSize)
+
+	exitC := make(chan error, 1)
+	s.ExitC = exitC
+
+	go s.handle(exitC)
 	return s
 }
 
-func (s *Stream) Start() (err error) {
+type Option func(*Stream)
+
+func ForwardUnsolicited(w io.Writer) Option {
+	return func(s *Stream) {
+		s.forward = w
+	}
+}
+
+func WithInternalBufSize(n int) Option {
+	return func(s *Stream) {
+		if n == 0 {
+			return
+		}
+		s.internalBufSize = n
+	}
+}
+
+func WithReceptionOptions(opts ...ReceptionOption) Option {
+	return func(s *Stream) {
+		s.globalParams.setup(nil, opts...)
+	}
+}
+
+func (s *Stream) StartReception(buf []byte, opts ...ReceptionOption) (err error) {
 	if s.eof {
 		err = io.EOF
 		return
@@ -42,29 +82,86 @@ func (s *Stream) Start() (err error) {
 	case err = <-s.errC:
 		s.eof = true
 	default:
-		s.buf = s.buf[:0]
+		s.curParams.setup(&s.globalParams, opts...)
+		s.buf = buf[:0:len(buf)]
 		s.req <- s.buf
 	}
 	return
 }
 
-func (s *Stream) Cancel() {
+func (s *Stream) CancelReception() {
 	s.req <- nil
 }
 
-func (s *Stream) Read(ctx context.Context, tMax, interframeTimeoutMax time.Duration) (buf []byte, err error) {
+type ReceptionOption func(*receptionParams)
+
+type receptionParams struct {
+	tMax                 time.Duration
+	interframeTimeoutMax time.Duration
+	frameValid           FrameValidationFunc
+	skipTimeout          bool
+	echo                 []byte
+}
+
+func (p *receptionParams) setup(dflt *receptionParams, opts ...ReceptionOption) *receptionParams {
+	if dflt != nil {
+		*p = *dflt
+	}
+	for _, o := range opts {
+		o(p)
+	}
+	return p
+}
+
+func WithInitialTimeout(t time.Duration) ReceptionOption {
+	return func(p *receptionParams) {
+		if t == 0 {
+			return
+		}
+		p.tMax = t
+	}
+}
+
+func WithInterframeTimeout(t time.Duration) ReceptionOption {
+	return func(p *receptionParams) {
+		p.interframeTimeoutMax = t
+	}
+}
+
+type FrameValidationFunc func(bnew []byte, msg []byte) bool
+
+func WithFrameValidation(f FrameValidationFunc) ReceptionOption {
+	return func(p *receptionParams) {
+		p.frameValid = f
+	}
+}
+
+func SkipTimeoutIfValid() ReceptionOption {
+	return func(p *receptionParams) {
+		p.skipTimeout = true
+	}
+}
+
+func WithLocalEcho(echo []byte) ReceptionOption {
+	return func(p *receptionParams) {
+		p.echo = echo
+	}
+}
+
+func (s *Stream) ReadFrame(ctx context.Context, opts ...ReceptionOption) (buf []byte, err error) {
 	if s.eof {
 		err = io.EOF
 		return
 	}
+	par := s.curParams.setup(&s.globalParams, opts...)
 	bufok := false
-	if s.CheckBytes == nil {
+	if par.frameValid == nil {
 		bufok = true
 	}
 	nto := 0
 	interframeTimeout := 1750 * time.Microsecond
-	ntoMax := int((interframeTimeoutMax + interframeTimeout - 1) / interframeTimeout)
-	timeout := time.NewTimer(tMax)
+	ntoMax := int((par.interframeTimeoutMax + interframeTimeout - 1) / interframeTimeout)
+	timeout := time.NewTimer(par.tMax)
 	nSkip := 0
 readLoop:
 	for {
@@ -72,8 +169,8 @@ readLoop:
 		case r := <-s.done:
 			nb := len(s.buf)
 			s.buf = r.data
-			if s.CheckBytes != nil && s.echo == nil {
-				bufok = s.CheckBytes(s.buf[nb:], s.buf[nSkip:])
+			if par.frameValid != nil && par.echo == nil {
+				bufok = par.frameValid(s.buf[nb:], s.buf[nSkip:])
 			}
 			if !timeout.Stop() {
 				<-timeout.C
@@ -85,42 +182,40 @@ readLoop:
 				return
 			}
 		reeval:
-			if s.echo != nil {
-				nEcho := len(s.echo)
+			if par.echo != nil {
+				nEcho := len(par.echo)
 				if len(s.buf) >= nEcho {
 					tail := s.buf[nEcho:]
-					if s.CheckBytes != nil && len(tail) != 0 {
-						bufok = s.CheckBytes(tail, s.buf[nSkip:])
+					if par.frameValid != nil && len(tail) != 0 {
+						bufok = par.frameValid(tail, s.buf[nSkip:])
 					}
-					if !bytes.Equal(s.buf[:nEcho], s.echo) {
+					if !bytes.Equal(s.buf[:nEcho], par.echo) {
 						err = modbus.ErrEchoMismatch
 						break readLoop
 					}
 					nSkip = nEcho
-					s.echo = nil
+					par.echo = nil
 					if len(tail) != 0 {
 						goto reeval
 					}
 				}
-				timeout.Reset(tMax)
+				timeout.Reset(par.tMax)
 				break
-			} else if s.MsgComplete != nil {
-				if s.MsgComplete(s.buf[nSkip:]) {
-					break readLoop
-				}
+			} else if bufok && par.skipTimeout {
+				break readLoop
 			}
-			if interframeTimeoutMax == 0 {
+			if par.interframeTimeoutMax == 0 {
 				break readLoop
 			}
 			nto = 0
 			timeout.Reset(interframeTimeout)
 
 		case <-timeout.C:
-			if s.echo != nil {
+			if par.echo != nil {
 				if len(s.buf[nSkip:]) != 0 {
-					err = modbus.ErrInvalidEchoLen
+					err = ErrInvalidEchoLen
 				} else {
-					err = modbus.ErrTimeout
+					err = ErrTimeout
 				}
 			} else if len(s.buf[nSkip:]) != 0 && !bufok && nto < ntoMax {
 				nto++
@@ -137,7 +232,7 @@ readLoop:
 
 	buf = s.buf[nSkip:]
 	if err == nil && len(buf) == 0 {
-		err = modbus.ErrTimeout
+		err = ErrTimeout
 	}
 	return
 }
@@ -147,7 +242,7 @@ type readResult struct {
 	err  error
 }
 
-func (s *Stream) handle(read ReadFunc, exitC chan<- error) {
+func (s *Stream) handle(exitC chan<- error) {
 	var termErr error
 	var dest []byte
 	var errC chan<- error
@@ -156,7 +251,13 @@ func (s *Stream) handle(read ReadFunc, exitC chan<- error) {
 	go func() {
 		var err error
 		for {
-			buf, err1 := read()
+			buf := s.internalBuf
+			n, err1 := s.r.Read(buf)
+			if err1 != nil {
+				buf = nil
+			} else {
+				buf = buf[:n]
+			}
 			data <- readResult{buf, err1}
 			<-data
 			if err1 != nil {
@@ -176,12 +277,18 @@ loop:
 			break loop
 		case dest = <-s.req:
 		case r, dataOk := <-data:
+			err := r.err
 			if dest != nil {
-				if r.err == nil {
+				if err == nil {
+					free := cap(dest) - len(dest)
+					if len(r.data) > free {
+						r.data = r.data[:free]
+						err = ErrOverflow
+					}
 					dest = append(dest, r.data...)
 				}
-			} else if s.Forward != nil {
-				s.Forward.Write(r.data)
+			} else if s.forward != nil {
+				s.forward.Write(r.data)
 			}
 			if dataOk {
 				data <- readResult{}
@@ -190,13 +297,13 @@ loop:
 			}
 			if dest != nil {
 				select {
-				case s.done <- readResult{dest, r.err}:
+				case s.done <- readResult{dest, err}:
 					if r.err != nil {
 						break loop
 					}
 				case b := <-s.req:
-					if s.Forward != nil {
-						s.Forward.Write(dest)
+					if s.forward != nil {
+						s.forward.Write(dest)
 					}
 					dest = b
 				}
@@ -208,4 +315,15 @@ loop:
 		}
 	}
 	close(s.done)
+}
+
+var ErrTimeout = Error("timeout")
+var ErrEchoMismatch = Error("local echo mismatch")
+var ErrInvalidEchoLen = Error("invalid local echo length")
+var ErrOverflow = Error("receive buffer overflow")
+
+type Error string
+
+func (e Error) Error() string {
+	return "serframe: " + string(e)
 }
